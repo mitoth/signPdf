319035d431905e417750d952e0c413902434bfe2
Mihai Toth <toth.mihai@live.com>
2021-01-02 23:43:53 +0200

    test projects

---
+++ RedactMyPdf.FileHandler.Tests/Conversion/ConversionTests.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using RedactMyPdf.FileHandler.Aspose.Conversion;
+using RedactMyPdf.FileHandler.Tests.Util;
+
+namespace RedactMyPdf.FileHandler.Tests.Conversion
+{
+    public class ConversionTests
+    {
+        private static readonly string OutputFolderPath = $"{Assembly.GetExecutingAssembly().Location}\\..\\..\\..\\PdfTestFiles\\TestOutput";
+
+        /// <summary>
+        /// Creates the output directory
+        /// </summary>
+        [OneTimeSetUp]
+        public void Initialize()
+        {
+            if (Directory.Exists(OutputFolderPath))
+            {
+                return;
+            }
+
+            // create the output test directory
+            Directory.CreateDirectory(OutputFolderPath);
+        }
+
+        [Test]
+        public async Task TestConvertPdfToIndividualJpegFile()
+        {
+            var testFilesParentDirectory = new DirectoryInfo($"{Assembly.GetExecutingAssembly().Location}").Parent
+                ?.Parent?.Parent?.Parent;
+            var inputPdfFilePath = $"{testFilesParentDirectory}\\PdfTestFiles\\test4PagesDocument.pdf";
+            var inputPdf = new FileInfo(inputPdfFilePath);
+
+            var fileBinary = await File.ReadAllBytesAsync(inputPdf.FullName);
+            await using var stream = new MemoryStream(fileBinary);
+
+            var conversionService = new PdfToJpgConverter();
+            var jpegFiles = await conversionService.ConvertAsync(stream, CancellationToken.None);
+            foreach (var jpegFile in jpegFiles)
+            {
+                await using var testFileStream = File.Create(Path.Combine(OutputFolderPath, $"jpegfile{jpegFiles.IndexOf(jpegFile) + 1}.jpeg"));
+                jpegFile.ImageStream.Seek(0, SeekOrigin.Begin);
+                await jpegFile.ImageStream.CopyToAsync(testFileStream);
+                Assert.IsTrue(await FileUtil.IsJpegFileSignature(testFileStream));
+            }
+        }
+    }
+}
---
+++ RedactMyPdf.Repository.Tests/BurnedDocumentRepositoryTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using RedactMyPdf.Core.Abstractions.Repositories;
+using RedactMyPdf.Core.Models;
+using RedactMyPdf.Core.Models.Draw;
+using RedactMyPdf.Repository.Mongo;
+using RedactMyPdf.Tests.Core;
+
+namespace RedactMyPdf.Repository.Tests
+{
+    public class BurnedDocumentRepositoryTests : BaseRepositoryTest
+    {
+        private readonly IBurnedDocumentRepository burnedDocumentRepository;
+
+        public BurnedDocumentRepositoryTests()
+        {
+            var loggerMock = new Mock<ILogger<BurnedDocumentRepository>>();
+            burnedDocumentRepository = new BurnedDocumentRepository(MongoClientConfiguration, loggerMock.Object);
+        }
+
+        [Test]
+        public async Task AddGetBurnedDocumentTest()
+        {
+            var shapes = new List<PageShapes>
+            {
+                new PageShapes(1, new List<Shape>
+                {
+                    new Rectangle(new Axis(2, 2), 2, 2, "some", 2, "other")
+                })
+            };
+
+            var documentId = Guid.NewGuid();
+            var addedDocument = new BurnedDocument(Guid.NewGuid(), documentId, "id", new DocumentShapes(shapes));
+            await burnedDocumentRepository.AddAsync(addedDocument, CancellationToken.None);
+            var retrievedDocument = await burnedDocumentRepository.GetAsync(documentId, CancellationToken.None);
+            Assert.AreEqual(addedDocument, retrievedDocument);
+        }
+    }
+}
