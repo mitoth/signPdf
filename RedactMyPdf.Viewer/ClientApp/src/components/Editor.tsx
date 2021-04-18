a6f8a346f85ac12359774764562c4791a988b269
Mihai Toth <toth.mihai@live.com>
2021-04-18 21:50:15 +0300

    popover

--- RedactMyPdf.Viewer/ClientApp/src/components/Editor.tsx
+++ RedactMyPdf.Viewer/ClientApp/src/components/Editor.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useRef } from 'react';
 import PageDrawStage from './PageDrawStage';
 import UploadService from '../services/FileUploadService';
 import Rectangle from '../interfaces/Rectangle';
@@ -22,6 +22,14 @@ import DeviceType from '../services/DeviceType';
 import ScreenSize from '../services/ScreenSize';
 import Backdrop from '@material-ui/core/Backdrop';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import StepContent from '@material-ui/core/StepContent';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Popover from '@material-ui/core/Popover';
+import Popper from '@material-ui/core/Popper';
 
 interface PageState {
     pages: Page[];
@@ -205,6 +213,19 @@ const Editor = (props: IProps): ReactElement => {
             zIndex: theme.zIndex.drawer + 1,
             color: '#fff',
         },
+        root: {
+            width: '100%',
+        },
+        button: {
+            marginTop: theme.spacing(1),
+            marginRight: theme.spacing(1),
+        },
+        actionsContainer: {
+            marginBottom: theme.spacing(2),
+        },
+        resetContainer: {
+            padding: theme.spacing(3),
+        },
     }));
 
     const classes = useStyles();
@@ -230,6 +251,57 @@ const Editor = (props: IProps): ReactElement => {
         fontSize = 'large';
     }
 
+    function getSteps() {
+        return ['Select campaign settings', 'Create an ad group', 'Create an ad'];
+    }
+
+    const [activeStep, setActiveStep] = React.useState(0);
+    const steps = getSteps();
+
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+
+    const handleReset = () => {
+        setActiveStep(0);
+    };
+
+    function getStepContent(step: number) {
+        switch (step) {
+            case 0:
+                return `For each ad campaign that you create, you can control how much
+                    you're willing to spend on clicks and conversions, which networks
+                    and geographical locations you want your ads to show on, and more.`;
+            case 1:
+                return 'An ad group contains one or more ads which target a shared set of keywords.';
+            case 2:
+                return `Try out different ad text to see what brings in the most customers,
+                    and learn how to enhance your ads using features like ad extensions.
+                    If you run into any problems with your ads, find out how to tell if
+                    they're running and how to resolve approval issues.`;
+            default:
+                return 'Unknown step';
+        }
+    }
+
+    const [anchorEl, setAnchorEl] = React.useState<(EventTarget & HTMLElement) | null>();
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popover' : undefined;
+    const handleClick = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
+        // setAnchorEl(event.currentTarget);
+        setAnchorEl(child1.current);
+        console.log(child1);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(undefined);
+    };
+    const child1 = useRef(null);
+
     return (
         <>
             <Backdrop className={classes.backdrop} open={isDownloadInProgress}>
@@ -253,6 +325,7 @@ const Editor = (props: IProps): ReactElement => {
                     Erase
                 </Button>
                 <Button
+                    onClick={handleClick}
                     variant="contained"
                     color="primary"
                     size={buttonSize}
@@ -282,10 +355,65 @@ const Editor = (props: IProps): ReactElement => {
                     <FileDownload downloadPath={downloadPath} onDownloadComplete={handleDownloadComplete} />
                 )}
             </ButtonGroup>
+
             <div style={{ backgroundColor: '#f4f6f5', cursor: addRectanglePressed ? 'crosshair' : '' }}>
                 <table className="center">
+                    <Popper
+                        id={id}
+                        open={open}
+                        anchorEl={anchorEl}
+                        // onClose={handleClose}
+                        // anchorOrigin={{
+                        //     vertical: 'bottom',
+                        //     horizontal: 'center',
+                        // }}
+                        // transformOrigin={{
+                        //     vertical: 'top',
+                        //     horizontal: 'center',
+                        // }}
+                    >
+                        <div className={classes.root}>
+                            <Stepper activeStep={activeStep} orientation="vertical">
+                                {steps.map((label, index) => (
+                                    <Step key={label}>
+                                        <StepLabel>{label}</StepLabel>
+                                        <StepContent>
+                                            <Typography>{getStepContent(index)}</Typography>
+                                            <div className={classes.actionsContainer}>
+                                                <div>
+                                                    <Button
+                                                        disabled={activeStep === 0}
+                                                        onClick={handleBack}
+                                                        className={classes.button}
+                                                    >
+                                                        Back
+                                                    </Button>
+                                                    <Button
+                                                        variant="contained"
+                                                        color="primary"
+                                                        onClick={handleNext}
+                                                        className={classes.button}
+                                                    >
+                                                        {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                                                    </Button>
+                                                </div>
+                                            </div>
+                                        </StepContent>
+                                    </Step>
+                                ))}
+                            </Stepper>
+                            {activeStep === steps.length && (
+                                <Paper square elevation={0} className={classes.resetContainer}>
+                                    <Typography>All steps completed - you&apos;re finished</Typography>
+                                    <Button onClick={handleReset} className={classes.button}>
+                                        Reset
+                                    </Button>
+                                </Paper>
+                            )}
+                        </div>
+                    </Popper>
                     <tbody>
-                        <tr className="height5percent"></tr>
+                        <tr className="height5percent" ref={child1}></tr>
                         {numberOfPages &&
                             numberOfPages > 0 &&
                             Array(numberOfPages)
@@ -307,6 +435,7 @@ const Editor = (props: IProps): ReactElement => {
                                     return (
                                         <React.Fragment key={i}>
                                             <tr className="height5percent">{i}</tr>
+                                            {/* <Paper elevation={5}> */}
                                             <tr>
                                                 <PageDrawStage
                                                     rectangles={rectangles}
@@ -321,6 +450,7 @@ const Editor = (props: IProps): ReactElement => {
                                                     touchStartEvent={touchStartEvent}
                                                 ></PageDrawStage>
                                             </tr>
+                                            {/* </Paper> */}
                                         </React.Fragment>
                                     );
                                 })}
