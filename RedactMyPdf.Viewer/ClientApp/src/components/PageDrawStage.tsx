73d9504c36eafba3e0395eb742d21917a521807d
toth.mihai <toth.mihai@live.com>
2021-09-20 23:06:42 +0300

    loading during image load

--- RedactMyPdf.Viewer/ClientApp/src/components/PageDrawStage.tsx
+++ RedactMyPdf.Viewer/ClientApp/src/components/PageDrawStage.tsx
@@ -3,6 +3,12 @@ import { Stage, Layer } from 'react-konva';
 import KonvaDrawLayer from './KonvaDrawLayer';
 import { MemoizedPageImage } from './PageImage';
 import Signature from '../interfaces/Signature';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import CircularProgress from '@mui/material/CircularProgress';
+import { Image } from 'react-konva';
+import useImage from 'use-image';
 
 interface IProps {
     signatures: Signature[];
@@ -18,21 +24,52 @@ interface IProps {
 const PageDrawStage = (props: IProps): ReactElement => {
     const url = `/api/v1/Document/${props.fileId}/page/${props.pageNumber}/file`;
 
-    return (
-        <>
-            <Stage width={props.width} height={props.height}>
-                <Layer>
-                    <MemoizedPageImage pageUrl={url} width={props.width} height={props.height} />
-                </Layer>
-                <KonvaDrawLayer
-                    signatures={props.signatures}
-                    setSignatures={props.setSignatures}
-                    selectedShapeId={props.selectedShapeId}
-                    setSelectedShapeId={props.setSelectedShapeId}
-                ></KonvaDrawLayer>
-            </Stage>
-        </>
-    );
+    const divStyle = {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: `${props.height}px`,
+    };
+
+    const [image, loadingState] = useImage(url);
+    if (image) {
+        image.width = props.width;
+        image.height = props.height;
+    }
+
+    if (loadingState == 'loaded') {
+        return (
+            <>
+                <Stage width={props.width} height={props.height}>
+                    <Layer>
+                        <Image image={image} preventDefault={false} />
+                    </Layer>
+                    <KonvaDrawLayer
+                        signatures={props.signatures}
+                        setSignatures={props.setSignatures}
+                        selectedShapeId={props.selectedShapeId}
+                        setSelectedShapeId={props.setSelectedShapeId}
+                    ></KonvaDrawLayer>
+                </Stage>
+            </>
+        );
+    } else {
+        return (
+            <>
+                <Box
+                    sx={{
+                        width: props.width,
+                        height: props.height,
+                        bgcolor: '#e7e4ef',
+                    }}
+                >
+                    <div id="mata" style={divStyle}>
+                        <CircularProgress color="secondary" />
+                    </div>
+                </Box>
+            </>
+        );
+    }
 };
 
 export default PageDrawStage;
