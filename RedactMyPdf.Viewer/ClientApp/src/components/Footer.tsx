5a52b7e44f14dfeb47e4f96d6a515a5f34e74282
toth.mihai <toth.mihai@live.com>
2021-10-24 16:58:57 +0300

    added policies

--- RedactMyPdf.Viewer/ClientApp/src/components/Footer.tsx
+++ RedactMyPdf.Viewer/ClientApp/src/components/Footer.tsx
@@ -1,16 +1,24 @@
 import React, { ReactElement } from 'react';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
+import Chip from '@mui/material/Chip';
 
 const Footer = (): ReactElement => (
     <footer className="footer">
         <Stack direction="row" justifyContent="center" alignItems="center" spacing={2}>
-            <Button variant="outlined" size="small">
-                Contact
-            </Button>
-            <Button variant="outlined" size="small">
-                Terms & Conditions
-            </Button>
+            <Chip label="Contact" component="a" href="" clickable />
+            <Chip
+                label="Terms & Conditions"
+                component="a"
+                href="https://www.websitepolicies.com/policies/view/U7jca8sa"
+                clickable
+            />
+            <Chip
+                label="Disclaimer"
+                component="a"
+                href="https://www.websitepolicies.com/policies/view/DE39uidA"
+                clickable
+            />
         </Stack>
     </footer>
 );
